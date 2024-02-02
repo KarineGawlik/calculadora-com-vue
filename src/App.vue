10f06e7811bf832b2723032f0ee8b7b4e7313a99
Karine <karinegawlik@hotmail.com>
2024-02-02 06:47:06 -0300

    resolvendo erro NaN

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Calculadora />
+    <Calculadora/>
   </div>
 </template>
 
--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,73 +1,71 @@
 <template>
-    <div id="app"> <!-- -->
+    <div id="app">
         <div class="calculadora">
             <p class="p">
-                Digite os números e selecione o operador que deseja calcular:
+            Digite os números e selecione o operador que deseja calcular:
             </p>
-            <input class="input" type="number" v-model="valor1" @input="calculate" /> <!-- Primeiro número (input) digitado pelo usuário -->
-            <!-- @input é acionado quando o usuário digita o número, com o método calculate para fazer o calculo -->
-            <select class="btn" v-model="operator" @change="calculate"> <!-- Abrindo uma caixinha para escolha da conta aritmética -->
-                <option value="add">+</option> <!-- Opções de escolhas para usuário: Mais, menos, multiplicação e divisão -->
-                <option value="subtract">-</option> 
-                <option value="multiply">*</option>
-                <option value="divide">÷</option>
+            <input class="input" type="number" v-model="valor1" @input="calculate" />
+            <select class="btn" v-model="operator" @change="calculate">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">÷</option>
             </select>
-            <input class="input" type="number" v-model="valor2" @input="calculate" /> <!-- Segundo número a ser preenchido pelo usuário -->
+            <input class="input" type="number" v-model="valor2" @input="calculate" />
             <div class="resultado">
-                Resultado = {{ result }} <!-- Mostrar resultado -->
+            Resultado = {{ result }}
             </div>
         </div>
-    </div>
-</template>
-
-<script>
-export default (await import('vue')).defineComponent({
-    data() {
+        </div>
+    </template>
+    
+    <script>
+    export default {
+        data() {
         return {
-        valor1: 0,
-        valor2: 0,
-        operator: 'add',
-        result: 0
+            valor1: 0,
+            valor2: 0,
+            operator: 'add',
+            result: 0
         };
-    },
-
-    methods: {/*Métodos para realizar os calculos selecionados */
+        },
+        methods: {
         calculate() {
             switch (this.operator) {
-                case 'add': // Adição
-                    this.result = parseFloat(this.num1) + parseFloat(this.num2);
-                    break;
-                case 'subtract': // Subtração
-                    this.result = parseFloat(this.num1) - parseFloat(this.num2);
-                    break;
-                case 'multiply': // Multiplicação
-                    this.result = parseFloat(this.num1) * parseFloat(this.num2);
-                    break;
-                case 'divide': // Divisão
-                    this.result = parseFloat(this.num1) / parseFloat(this.num2);
-                    break;
-                default: // Se nenhuma operação válida for selecionada, o resultado é 0
-                    this.result = 0;
+            case 'add':
+                this.result = parseFloat(this.valor1) + parseFloat(this.valor2);
+                break;
+            case 'subtract':
+                this.result = parseFloat(this.valor1) - parseFloat(this.valor2);
+                break;
+            case 'multiply':
+                this.result = parseFloat(this.valor1) * parseFloat(this.valor2);
+                break;
+            case 'divide':
+                this.result = parseFloat(this.valor1) / parseFloat(this.valor2);
+                break;
+            default:
+                this.result = 0;
             }
         }
-    },
-})
-</script>
-
-<style scoped>
+        }
+    };
+    </script>
+    
+    <style scoped>
     .calculadora {
         margin: 0 auto;
         width: 200px;
         padding: 20px;
         font-size: 20px;
-        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         border: 1px solid #ccc;
         background-color: rgb(3, 81, 88);
     }
     .input {
         margin: 10px;
         padding: 10px;
-        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
     .resultado {
         margin-top: 10px;
@@ -75,7 +73,6 @@ export default (await import('vue')).defineComponent({
         color: #fff;
         margin-left: 22px;
     }
-
     .btn {
         width: 60px;
         margin-left: 60px;
@@ -83,7 +80,6 @@ export default (await import('vue')).defineComponent({
     .p {
         margin-left: 10px;
         color: #fff;
-        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
-    
     </style>
